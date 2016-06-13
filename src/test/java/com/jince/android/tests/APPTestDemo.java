b11a4db406f67942a348fba6e4492d0138139aba
Makexxx <278673352@qq.com>
2016-06-13 17:04:41 +0800

    Add config for surefire plugin
    add suiteFile parameter in pom.xml

--- src/test/java/com/jince/android/tests/APPTestDemo.java
+++ src/test/java/com/jince/android/tests/APPTestDemo.java
@@ -25,24 +25,6 @@ public class APPTestDemo extends BaseTest{
     	driver.navigate().back();
     	boolean result = minePage.openMinePage();
     	Assert.assertTrue(result);
-//    	boolean result = minePage.openMinePage();
-    	Thread.sleep(6000);
-//    	Assert.assertTrue(result);
-//    	WebElement mine = driver.findElementById("com.jincehuangjin.jindashi:id/tv_wd");
-//    	Assert.assertTrue(mine.isDisplayed());
-//    	mine.click();
-//    	WebElement nicknameBtn = driver.findElementById("com.jincehuangjin.jindashi:id/mine_user_nickname_textview");
-//    	Assert.assertTrue(nicknameBtn.isDisplayed());
-//    	nicknameBtn.click();
-//    	WebElement userName = driver.findElementById("com.jincehuangjin.jindashi:id/login_account_et");
-//    	WebElement passWord = driver.findElementById("com.jincehuangjin.jindashi:id/login_password_et");
-//    	userName.sendKeys(UIConfig.phone);
-//    	passWord.sendKeys(UIConfig.password);
-//    	WebElement loginBtn = driver.findElementById("com.jincehuangjin.jindashi:id/user_login_btn");
-//    	Assert.assertTrue(loginBtn.isDisplayed());
-//    	loginBtn.click();
-    	
-        
-        System.out.println("dsds");
+    	Thread.sleep(6000);	
     }
 }
